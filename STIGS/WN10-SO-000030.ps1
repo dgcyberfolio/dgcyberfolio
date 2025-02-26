02f5bb046b104cc209518b5d92fc774cde65a314
David Gilmore <davidgilmore55@gmail.com>
2025-02-26 14:04:08 -0600

    Create WN10-SO-000030.ps1

---
+++ STIGS/WN10-SO-000030.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : David Gilmore
+    LinkedIn        : linkedin.com/in/david-gilmore1/
+    GitHub          : github.com/dgcyberfolio
+    Date Created    : 2024-02-26
+    Last Modified   : 2024-02-26
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000030
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000030.ps1 
+#>
+
+
+# Define the registry path and value
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$valueName = "SCENoApplyLegacyAuditPolicy"
+$value = 1
+
+# Check if the registry path exists
+if (Test-Path $regPath) {
+    # Set the registry value to 1
+    Set-ItemProperty -Path $regPath -Name $valueName -Value $value
+    Write-Host "Registry value set successfully."
+} else {
+    Write-Host "Registry path does not exist."
+}
